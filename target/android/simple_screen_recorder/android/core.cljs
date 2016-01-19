7705e0c761df682d92e4b470ebcefebc59b89178
Eissek <sane.gez@gmail.com>
2016-01-19 23:17:33 +0000

    using drawerlayout with view inside it

--- target/android/simple_screen_recorder/android/core.cljs
+++ target/android/simple_screen_recorder/android/core.cljs
@@ -18,19 +18,21 @@
 (def logo-img (require-img "./images/cljs.png"))
 (def drawer-layout-object (.-DrawerLayoutAndroid js/React))
 (def drawer-layout-and (r/adapt-react-class drawer-layout-object))
+
 ;; (def drawer-position (r/adapt-react-class [drawer-layout-and DrawerPosition]))
 (def drawer-left (.-positions drawer-layout-object))
 (def touch-opacity (r/adapt-react-class (.-TouchableOpacity js/React)))
 (def alert-dialog (.-Alert js/React))
 
-(defn nav-view
-  []
+(defn nav-view []
   (fn []
     [view {:style
            {:flex 1
             :backgroundColor "#FFF"}}
      [text {:style
             {:fontSize 10
+             :margin 10
+             :color "#000"
              :textAlign "left"}} "Test Drawer"]]))
 
 (defn drawer-menu []
@@ -44,39 +46,51 @@
             :fontSize 15
             :textAlign "right"}} "Hello"]]])
 
-;; (def drawer-wrapper
+(defn show-text []
+  (fn []
+    (.alert  alert-dialog "Title here" "My Message")))
+
+
+;; (defn bars-menu []
+;;   [touch-opacity {:onPress (my-drawer-menu)}
+;;    [icon {:name "bars"
+;;           :size 15
+;;           :color "#000"
+;;           :style {:margin-left 0}
+;;           ;; :on-click #(.openDrawer [drawer-menu])
+;;           }]]
+;;   ;; (this-as this )
+;;   )
+
+(defn test-this []
+  (.alert  alert-dialog "Title heress" "Test Messassssge"))
+
+;; (def bars-menu
+;;   ;; Should return drawer-menu
 ;;   (with-meta drawer-menu
 ;;     {:component-did-mount
 ;;      (fn [this]
-;;        {:on-click #(.openDrawer this)}
-;;        ;; (js/console.log "TESTING")
-;;        )}))
+;;        (.alert  alert-dialog "Title heress" "My Message"))
+;;      }))
 
-;; (defn test-menu []
-;;   [icon {:name "bars"
-;;               :size 15
-;;               :color "#000"
-;;               :style {:margin-left 0}
-;;          ;; :on-click #(.openDrawer this)
-;;               }])
-
-(defn show-text []
-  (fn []
-    ;; [text {:style {:color "#FFF"}} "HEEEEEEEEEEY"]
-    ;; [view [text {:style {:color "#FFF"}}]]
-    ;; (js/console.log "HEEEEEEEEEEY")
-    (.alert  alert-dialog "Title here" "My Message")))
 
 (defn bars-menu []
-  (this-as this
-           [touch-opacity {:onPress (show-text)}
-            [icon {:name "bars"
-                   :size 15
-                   :color "#000"
-                   :style {:margin-left 0}
-                   ;; :on-click #(.openDrawer [drawer-menu])
-                   ;; :on-click #([text {:style {:color "#FFF"}}])
-                   }]]))
+  (r/create-class
+   {:component-did-mount
+    (test-this)
+    :display-name "bars-menu"
+    :reagent-render
+    (fn []
+      ;; (.alert  alert-dialog "Title heress" "My Message")
+      )}))
+
+(defn bars [drawer]
+  [touch-opacity {:onPress bars-menu}
+   [icon {:name "bars"
+          :size 15
+          :color "#000"
+          :style {:margin-left 0}
+          }]])
 ;; (def bars-menu
 ;;   (with-meta drawer-menu
 ;;     {:component-did-mount
@@ -104,7 +118,8 @@
    ;;        :style {:margin-left 0}
    ;;        :on-click #([text {:style {:color "#FFF"}}])
    ;;        }]
-   [bars-menu]
+   ;; [bars-menu]
+   [bars bars-menu]
    [text {:style {:color "#000"
                   :textAlign "center"
                   :fontSize 15}}
@@ -114,35 +129,71 @@
           :color "#000"
           :style {:margin-right 10}}]])
 
+(defn my-view []
+  [view {:style {:flex 1 :backgroundColor "#FFF"}}
+   [text {:style {:fontSize 10
+                  :margin 10
+                  :color "#000"
+                  :textAlign "left"}} "Test Drawer"]])
+
+(defn the-view []
+  (r/create-class
+   {;; :component-did-mount
+    ;; (test-this)
+    :display-name "the-view"
+    :reagent-render
+    [view {:style {:flex 1 :backgroundColor "#1E750E"}}
+     [text {:style {:fontSize 10
+                    :margin 5
+                    :color "#000"
+                    :textAlign "left"}} "Test Drawer WOOOOOOOOOO HSHSHSHHSSNSN"]]
+    }))
+
 (defn widget []
   (let [greeting (subscribe [:get-greeting])]
     (fn []
       ;; Body view
-      [view {:style
-             {:flex 1
-              :flexDirection "column"
-              :backgroundColor "#FFF"
-              :margin 0
-              :alignItems "center"
-              ;; :color "#000"
-              ;; :padding 10
-              }}
-       ;; Header-view
-       [header-view]
-       ;; [drawer-wrapper]
-       [drawer-menu]
-       ;; [drawer-layout-and {:drawerWidth 300
-       ;;                      :drawerPosition (.-Left drawer-left)
-       ;;                      :renderNavigationView nav-view}
-       ;;  [view {:style
-       ;;         {:flex 1 :alignItems "center"}}
-       ;;   [text {:style
-       ;;          {:margin 10
-       ;;           :fontSize 15
-       ;;           :textAlign "right"}} "Hello"]]]
-       
-       
-       ])))
+      ;; [view {:style
+      ;;        {:flex 1
+      ;;         :flexDirection "column"
+      ;;         :backgroundColor "#FFF"
+      ;;         :margin 0
+      ;;         :alignItems "center"
+      ;;         ;; :color "#000"
+      ;;         ;; :padding 10
+      ;;         }}
+      ;;  ;; Header-view
+      ;;  ;; [header-view]
+      ;;  ]
+
+      ;; (let [drawer (r/adapt-react-class (.-DrawerLayoutAndroid js/React))]
+      ;;   [drawer {:drawerWidth 5
+      ;;            :drawerPosition (.-Left (.-positions drawer))
+      ;;            ;; (.-Left (.-positions drawer-layout-object))
+      ;;            :renderNavigationView nav-view}
+      ;;    [view {:style {:flex 1 :alignItems "center"}}
+      ;;     [text {:style {:margin 10
+      ;;                    :color "#000"
+      ;;                    :fontSize 15
+      ;;                    :textAlign "right"}} "HELLO WORLD"]]
+      ;;    ])
+
+      
+
+      
+      
+      [drawer-layout-and {:drawerWidth 10
+                          :drawerPosition js/React.DrawerLayoutAndroid.positions.Left
+                          :renderNavigationView the-view }
+               [view {:style
+                      {:flex 1 :alignItems "center"}}
+                [text {:style
+                       {:margin 2
+                        :color "#000"
+                        :fontSize 15
+                        :textAlign "right"}} "Hello"]]]
+      )))
+
 
 
 ;; [image {:source logo-img :style  {:width 30 :height 30 :marginBottom 10}}]
